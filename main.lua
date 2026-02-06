dbffc20b293ecb8851600862bc959907f72f872c
Im-yTrusk <gabrielnoahj7@gmail.com>
2026-02-05 21:13:25 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,10 @@
 local placeId = game.PlaceId
 local player = game.Players.LocalPlayer
-if placeId ~= 109983668079237 then
-    player:Kick("Game Not Supported!")
-    else
+if placeId == 109983668079237 then
     getgenv().SCRIPT_KEY = ""
-    loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/76d354569c8022009a4aead8a6863abf74009a9a73ec81028ac4c08158afcb68/download"))()
+    loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/76d354569c8022009a4aead8a6863abf74009a9a73ec81028ac4c08158afcb68/download"))() 
+else if placeId == 111989938562194 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Im-yTrusk/only/refs/heads/main/brainrotEvolution.lua"))()
+    else
+        player:Kick("Game Not Supported!")
 end
