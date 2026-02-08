8da4be06b30c3859853f0fe841cd274cdcabdf3c
Im-yTrusk <gabrielnoahj7@gmail.com>
2026-02-07 22:57:56 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,12 +2,11 @@ local placeId = game.PlaceId
 local player = game.Players.LocalPlayer
 if placeId == 109983668079237 then
     getgenv().SCRIPT_KEY = ""
-    loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/76d354569c8022009a4aead8a6863abf74009a9a73ec81028ac4c08158afcb68/download"))() 
-else if placeId == 111989938562194 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Im-yTrusk/only/refs/heads/main/brainrotEvolution.lua"))()
-    else if placeId == 3823781113 then 
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Im-yTrusk/only/refs/heads/main/sabersimulator.lua"))()
-    else
-        player:Kick("Game Not Supported!")
-    end
+    loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/76d354569c8022009a4aead8a6863abf74009a9a73ec81028ac4c08158afcb68/download"))()
+elseif placeId == 111989938562194 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Im-yTrusk/only/refs/heads/main/brainrotEvolution.lua"))()
+elseif placeId == 3823781113 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Im-yTrusk/only/refs/heads/main/sabersimulator.lua"))()
+else
+    player:Kick("Game Not Supported!")
 end
