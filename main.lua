aa7714a4a5f938c741ca896c577cc970fdac7a07
Im-yTrusk <gabrielnoahj7@gmail.com>
2026-02-08 21:38:38 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,6 +7,9 @@ elseif placeId == 111989938562194 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Im-yTrusk/only/refs/heads/main/brainrotEvolution.lua"))()
 elseif placeId == 3823781113 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Im-yTrusk/only/refs/heads/main/sabersimulator.lua"))()
+elseif placeId == 119987266683883 then
+    getgenv().SCRIPT_KEY = ""
+    loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/4d5d494d8b936b2ad86c7ba02230d2b56df49e7677839d73abcfa1cef54bd5f8/download"))()
 else
     player:Kick("Game Not Supported!")
 end
