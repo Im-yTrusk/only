d036dd25a9fa68cc6aee62e37d7bce234b273c44
Im-yTrusk <gabrielnoahj7@gmail.com>
2026-05-12 16:20:32 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,6 +4,8 @@ if placeId == 3823781113 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Im-yTrusk/only/refs/heads/main/sabersimulator.lua"))()
 elseif placeId == 125003919504672 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Im-yTrusk/only/refs/heads/main/myknife.lua"))()
+elseif placeId == 134963296781989 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Im-yTrusk/only/refs/heads/main/clover.lua"))()
 else
     player:Kick("Game Not Supported! Check our Discord: https://discord.gg/jak6yYH8hu")
 end
