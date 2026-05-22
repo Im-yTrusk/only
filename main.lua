0103d8a170d044f509fc67aed6b73d457441f8f6
Im-yTrusk <gabrielnoahj7@gmail.com>
2026-05-21 23:38:17 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -8,6 +8,8 @@ elseif placeId == 134963296781989 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Im-yTrusk/only/refs/heads/main/clover.lua"))()
 elseif placeId == 142823291 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Im-yTrusk/only/refs/heads/main/mm2.lua"))()
+elseif placeId == 105342279726230 or placeId == 106357754220636 or placeId == 95740619340896 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Im-yTrusk/only/refs/heads/main/world2.lua"))()
 else
     player:Kick("Game Not Supported! Check our Discord: https://discord.gg/jak6yYH8hu")
 end
